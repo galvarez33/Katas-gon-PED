edd5a6f1fe799ecb650ccc844da65e28e158f069
root <root@DESKTOP-22T8HCB>
2023-05-13 01:54:45 +0200

    Dni -> Valida el dni completo

--- dni/dni.py
+++ dni/dni.py
@@ -14,9 +14,33 @@ class Dni:
         ultimo_caracter = self.dni[-1]
         es_letra = ultimo_caracter.isalpha()
         if es_letra:
-            return True
+            primeros_8_caracteres = self.dni[:8]
+            es_numero = primeros_8_caracteres.isdigit()
+            if es_numero:
+                return True
+            else:
+                raise SyntaxError("est edni no tiene letr al final")
+
         else:
             raise SyntaxError("est edni no tiene letr al final")
+        
+
+    def validar_calculo(self):
+        l =["T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"]
+        resto = int(self.dni[:8]) % 23
+        letra = l[resto]
+        dni_final = f"{self.dni[:8]}{letra}"
+        return dni_final
+    
+
+    
+
+if __name__ == "__main__":
+    entrada = input("Introduzca un dni 8 Numeros y 1 LETRA: ")
+    dni = Dni(entrada)
+    dni.validar_calculo()
+       
+
         
         
             
--- dni/test_dni.py
+++ dni/test_dni.py
@@ -17,7 +17,7 @@ def test_longitud_erronea():
     except ValueError:
         assert True
 
-def test_validar_formato():
+def test_validar_formato_letra():
     dni = "12345678N"
     obj = Dni(dni)
     assert obj.validar_formato() == True
@@ -29,3 +29,22 @@ def test_formato_letra_erroneo():
         assert False, "No deberia pasar"
     except SyntaxError:
         assert True
+
+def test_formato_numero_erroneo():
+    dni = "1234n67N"
+    try:
+        Dni(dni).validar_formato()
+        assert False, "No deberia pasar"
+    except SyntaxError:
+        assert True
+
+
+def test_validar_calculo():
+    dni = "12345678Z"
+    obj = Dni(dni)
+    assert obj.validar_calculo() == dni
+
+def test_validar_calculo():
+    dni = "02454678A"
+    obj = Dni(dni)
+    assert obj.validar_calculo() == dni

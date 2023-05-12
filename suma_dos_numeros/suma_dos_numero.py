26cdcab8b608ff33232e0e861b2ae0f50a273808
root <root@DESKTOP-22T8HCB>
2023-05-12 19:43:58 +0200

    Refactor falla

--- suma_dos_numeros/suma_dos_numero.py
+++ suma_dos_numeros/suma_dos_numero.py
@@ -1,11 +1,23 @@
 
 class SumaDosNumeros:
 
+
+    def es_entero(a,b):
+       try:
+          float(a)
+          float(b)
+          return True
+       except:
+          return False
+
+
     def suma(a, b):
-        try:
-          a = float(a)
-          b = float(b)
-          return a + b 
-        except:
+        if not es_entero(a,b):
            raise ValueError("Valor no valido")
+
+        a = float(a)
+        b = float(b)
+        return a + b 
+    
+           
         

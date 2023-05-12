2460bdf8ff51ff3583c466c68f3642fd554fdc22
root <root@DESKTOP-22T8HCB>
2023-05-12 20:49:40 +0200

    Sumador -> test falla

--- suma_recursiva/test_suma_recursiva.py
+++ suma_recursiva/test_suma_recursiva.py
@@ -6,3 +6,12 @@ def test_crear_valor():
     objeto = SumaRecursiva(valor)
 
     assert objeto.valor == valor
+
+def test_suma_recursiva():
+    n = 5
+    resultado_esperado = 15 #1+2+3+4+5
+    sumador = SumaRecursiva(n)
+    resultado = sumador.suma_recursiva()
+
+    assert resultado_esperado == resultado
+

1a95def5f33b502409448b6c875aa523bae4a1a1
root <root@DESKTOP-22T8HCB>
2023-05-13 11:58:22 +0200

    Rectangulo -> test 3, validar tupla punto 1 pasa

--- simulacro_rectangulo/rectangulo.py
+++ simulacro_rectangulo/rectangulo.py
@@ -1,5 +1,10 @@
 class Rectangulo:
 
     def __init__(self, punto1, punto2):
-        self.punto1 = punto1
-        self.punto2 = punto2
+        if isinstance(punto1, tuple):
+            self.punto1 = punto1
+            self.punto2 = punto2
+        else:
+            raise ValueError("Formato de punto incorrecto")
+
+    
--- simulacro_rectangulo/test_rectangulo.py
+++ simulacro_rectangulo/test_rectangulo.py
@@ -11,3 +11,14 @@ def test_guarda_punto2():
     punto2 = (2,3)
     obj = Rectangulo(punto1, punto2)
     assert obj.punto2 == punto2
+
+def test_punt1_debe_ser_tupla():
+    punto1 = (3)
+    punto2 = (2,3)
+    try:
+        obj = Rectangulo(punto1, punto2)
+        isinstance(obj.punto1, tuple)
+        assert False
+    except ValueError:
+        assert True
+

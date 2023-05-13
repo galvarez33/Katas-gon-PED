e062d6ead211b25d58e9874626cfa3e2a1142f17
root <root@DESKTOP-22T8HCB>
2023-05-13 12:00:40 +0200

    Rectangulo -> test 4, validar tupla punto 2 pasa

--- simulacro_rectangulo/rectangulo.py
+++ simulacro_rectangulo/rectangulo.py
@@ -1,7 +1,7 @@
 class Rectangulo:
 
     def __init__(self, punto1, punto2):
-        if isinstance(punto1, tuple):
+        if isinstance(punto1, tuple) and isinstance(punto2, tuple):
             self.punto1 = punto1
             self.punto2 = punto2
         else:

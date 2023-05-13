fdc556c92207de82b2a2b5e160577046308b5297
root <root@DESKTOP-22T8HCB>
2023-05-13 11:49:47 +0200

    Rectangulo -> segundo test falla

--- simulacro_rectangulo/test_rectangulo.py
+++ simulacro_rectangulo/test_rectangulo.py
@@ -4,3 +4,8 @@ def test_guarda_punto1():
     punto1 = (2,3)
     obj = Rectangulo(punto1)
     assert obj.punto1 == punto1
+
+def test_guarda_punto2():
+    punto2 = (2,3)
+    obj = Rectangulo(punto2)
+    assert obj.punto2 == punto2
